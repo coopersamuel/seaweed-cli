c670464001db43d4b3bfc8f94494d2f227cdde7d
Samuel Cooper <cooper.samuel@outlook.com>
2018-08-08 15:41:36 -0400

    added arg parser

--- seaweed/__main__.py
+++ seaweed/__main__.py
@@ -2,16 +2,24 @@
     This is the entry point for the CLI
 '''
 
-import sys
-from .surfline import search
+import argparse
+from .surfline import search, spotSuggestion
 
 def main():
+    # This will be the only state kept for the application
+    state = {
+        "spotId": '',
+        "forecastType": ''
+    }
+
     # Parse args passed to the cli
-    args = sys.argv[1:]
-    print('count of args :: {}'.format(len(args)))
-    for arg in args:
-        print('passed argument :: {}'.format(arg))
-        search(arg)
+    parser = argparse.ArgumentParser(description="Get a forecast!")
+    parser.add_argument('forecastType', help='when do you want to go surfing - today, tomorrow, weekend or forecast')
+    parser.add_argument('spot', help='where do you want to surf')
+
+    args = parser.parse_args()
+
+    print(args.forecastType, args.spot)
 
 if __name__ == '__main__':
     main()
--- seaweed/surfline.py
+++ seaweed/surfline.py
@@ -9,7 +9,30 @@ def search(searchTerm):
     }
 
     response = requests.get("https://services.surfline.com/search/site", params=parameters)
-    data = response.json()
-    # print(data[0]["suggest"])
+    return response.json()
+
+def spotSuggestion(data):
+    # Print out the spot suggestions received from the surfline API
     for entry in data[0]["suggest"]["spot-suggest"][0]["options"]:
         print(entry["text"])
+
+def getForecastDays(forecastType):
+    # These are the types of forecasts offered based on the users preference
+    forecastTypes = {
+        "forecast": 7,
+        "today": 1,
+        "tomorrow": 2,
+        "weekend": 3, # TODO - Figure out how to get the weekend forecast
+    }
+
+    return forecastTypes.forecastType
+
+def waveForecast(spotId, days):
+    # Set up parameters dictionary
+    parameters = {
+        "spotId": spotId,
+        "days": days
+    }
+
+    response = requests.get("https://services.surfline.com/kbyg/spots/forecasts/wave", params=parameters)
+    return response.json()
